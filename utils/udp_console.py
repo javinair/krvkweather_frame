a1d578bdd34e43ce6cc58792f29f1c93d76edbb5
Javier Navarro <javinair@gmail.com>
2024-12-04 18:49:49 +0100

    Added new fonts and UDPConsole is updated

--- utils/udp_console.py
+++ utils/udp_console.py
@@ -6,10 +6,20 @@ UDP_PORT = 12345
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
+sock.settimeout(1.0)  # Timeout de 1 segundo
 
 print(f"Escuchando en {UDP_IP}:{UDP_PORT}...")
 
-while True:
-    data, _ = sock.recvfrom(1024)
-    timestamp = datetime.now().strftime("%d/%m/%y %H:%M:%S")
-    print(f"[{timestamp}] {data.decode()}")
+try:
+    while True:
+        try:
+            data, _ = sock.recvfrom(1024)
+            timestamp = datetime.now().strftime("%d/%m/%y %H:%M:%S")
+            print(f"[{timestamp}] {data.decode()}")
+        except socket.timeout:
+            # Si no hay datos, simplemente pasa
+            pass
+except KeyboardInterrupt:
+    print("\nApagando monitor UDP...")
+finally:
+    sock.close()
